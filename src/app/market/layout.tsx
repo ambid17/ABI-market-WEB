a6151ecf698ce1dec6981c5c822de42a5735647a
ambid17 <ambid17@gmail.com>
2025-10-15 00:04:42 -0500

    remove sub-folder

---
+++ src/app/market/layout.tsx
@@ -0,0 +1,11 @@
+export default async function MarketLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <main className="flex-1">{children}</main>
+    </div>
+  );
+}
